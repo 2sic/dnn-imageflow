770fd65e856652e6f48315a32752cba9601eca12
Tonći Vatavuk <tonci@sistemi.hr>
2023-07-12 17:18:32 +0200

    v1.2.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("01.02.00")]
-[assembly: AssemblyFileVersion("01.02.00")]
+[assembly: AssemblyVersion("01.02.01")]
+[assembly: AssemblyFileVersion("01.02.01")]
